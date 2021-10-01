5ed5d9d3aa45c4d260bf391727be933346d5bfea
Stranger Dennis <dxxenos@gmail.com>
2021-10-01 15:16:16 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@
 #define CREATE_BUTTON 4
 #define LI 48
 #define W 10
-#define K 3
+#define K 24
 
 using namespace std;
 
@@ -28,7 +28,6 @@ struct codeword
 	int page_loc;
 	int line_loc;
 	int word_loc;
-
 };
 
 struct node
@@ -39,21 +38,23 @@ struct node
 	int word_no;
 	int is_in_password;
 	struct node* next;
-
 }*head;
 
 //Declaration of: 
 //pages: number of pages in generated book
+//hpages: char array with the value of pages
 //BIP_words: catalogue of available words. Found in catalog.h
 //wallet_key[K]: the words in the user's code
 //existing_word_flag: used for checking whether or not the word provided by the user is in the list of available words
 //cw: current word counter. Used for location check.
 
 int pages = 0;
+char hpages[3];
 string BIP_words[2048];
 codeword wallet_key[K];
 int existing_word_flag = -1;
 int cw = -1;
+
 
 //Each word in the book is a node, therefore insert_at_end is used to fill the book
 void insert_at_end(int page, int line, int word_loc, string new_word)
@@ -92,39 +93,18 @@ void AddMenus(HWND);
 void AddControls(HWND);
 void loadImages();
 void generate_book();
-void place_words();
-void write_to_txt(); 
 void get_keys();
+void place_words();
+void write_to_txt();
 
 //Define handlers
-HWND hKey1, hPage1, hLine1, hWord1;
-HWND hKey2, hPage2, hLine2, hWord2;
-HWND hKey3, hPage3, hLine3, hWord3;
-HWND hKey4, hPage4, hLine4, hWord4;
-HWND hKey5, hPage5, hLine5, hWord5;
-HWND hKey6, hPage6, hLine6, hWord6;
-HWND hKey7, hPage7, hLine7, hWord7;
-HWND hKey8, hPage8, hLine8, hWord8;
-HWND hKey9, hPage9, hLine9, hWord9;
-HWND hKey10, hPage10, hLine10, hWord10;
-HWND hKey11, hPage11, hLine11, hWord11;
-HWND hKey12, hPage12, hLine12, hWord12;
-HWND hKey13, hPage13, hLine13, hWord13;
-HWND hKey14, hPage14, hLine14, hWord14;
-HWND hKey15, hPage15, hLine15, hWord15;
-HWND hKey16, hPage16, hLine16, hWord16;
-HWND hKey17, hPage17, hLine17, hWord17;
-HWND hKey18, hPage18, hLine18, hWord18;
-HWND hKey19, hPage19, hLine19, hWord19;
-HWND hKey20, hPage20, hLine20, hWord20;
-HWND hKey21, hPage21, hLine21, hWord21;
-HWND hKey22, hPage22, hLine22, hWord22;
-HWND hKey23, hPage23, hLine23, hWord23;
-HWND hKey24, hPage24, hLine24, hWord24;
+HWND hKey[24];
+HWND hPage[24];
+HWND hLine[24];
+HWND hWord[24];
 HWND hname;
 HWND hnpages;
 HWND hOut;
-
 HMENU hMenu;
 HBITMAP hCreateImage;
 
@@ -179,7 +159,7 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 			//Variables from forms to  char
 			char out[2400];
 
-			char hpages[3];
+			//char hpages[3];
 			GetWindowText(hnpages, hpages, 3);
 			int pages = atoi(hpages);
 
@@ -239,7 +219,7 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 //Generating a book of random BIP words. Page amount is determined by the user.
 void generate_book()
 {
-	char hpages[3];
+	//char hpages[3];
 	GetWindowText(hnpages, hpages, 3);
 	int pages = atoi(hpages);
 
@@ -267,27 +247,26 @@ void generate_book()
 
 void get_keys()
 {
-	cw = 1;
-	char key1c[10]; //10 char array declaration
-	GetWindowText(hKey1, key1c, 10); //parse values from handle to char array
-	string key1; //declare string
-	key1 += key1c; //add values to string
-	wallet_key[cw].word = key1;
 
-	char page1c[3]; //3 char array declaration
-	GetWindowText(hKey1, page1c, 3); //parse values from handle to char array
-	int page1 = atoi(page1c); //array to int
-	wallet_key[cw].page_loc = page1;
+	char key1c[K][10];
+	char page1c[K][4];
+	char line1c[K][3];
+	char word1c[K][3];
 
-	char line1c[3]; //3 char array declaration
-	GetWindowText(hKey1, line1c, 3); //parse values from handle to char array
-	int line1 = atoi(line1c); //array to int
-	wallet_key[cw].line_loc = line1;
+	for (int i = 0; i < K; i++)
+	{
+		GetWindowText(hKey[i], key1c[i], 10);
+		wallet_key[i].word = key1c[i];
 
-	char word1c[3]; //3 char array declaration
-	GetWindowText(hKey1, word1c, 3); //parse values from handle to char array
-	int word1 = atoi(word1c); //array to int
-	wallet_key[cw].word_loc = word1;
+		GetWindowText(hPage[i], page1c[i], 4);
+		wallet_key[i].page_loc = atoi(page1c[i]);
+
+		GetWindowText(hLine[i], line1c[i], 3);
+		wallet_key[i].line_loc = atoi(line1c[i]);
+
+		GetWindowText(hWord[i], word1c[i], 3);
+		wallet_key[i].word_loc = atoi(word1c[i]);
+	}
 
 	MessageBeep(MB_ICONINFORMATION);
 }
@@ -331,10 +310,10 @@ void place_words()
 
 void write_to_txt()
 {
-	char hpages[3]; //3 char array declaration
+	//char hpages[3]; //3 char array declaration
 	GetWindowText(hnpages, hpages, 3); //parse values from handle to char array
 	int pages = atoi(hpages); //convert to integer
-	
+
 	char booknamec[100]; //100 char array declaration
 	GetWindowText(hname, booknamec, 100); //parse values from handle to char array
 	string bookname; //declare string
@@ -435,114 +414,71 @@ void AddControls(HWND hWnd)
 	CreateWindowW(L"static", L"24: ", WS_VISIBLE | WS_CHILD, 340, 260, 100, 20, hWnd, NULL, NULL, NULL);
 
 	//Left Key Input
-	hKey1 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 38, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey2 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 58, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey3 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 78, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey4 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 98, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey5 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 118, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey6 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 138, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey7 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 158, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey8 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 178, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey9 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 198, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey10 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 218, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey11 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 238, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey12 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, 258, 100, 20, hWnd, NULL, NULL, NULL);
+	int handleYpos = 38;
+	for (int i = 0; i < 12; i++)
+	{
+		hKey[i] = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 46, handleYpos, 100, 20, hWnd, NULL, NULL, NULL);
+		handleYpos += 20;
+	}
+
 	//Right Key Input
-	hKey13 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 38, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey14 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 58, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey15 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 78, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey16 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 98, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey17 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 118, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey18 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 138, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey19 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 158, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey20 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 178, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey21 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 198, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey22 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 218, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey23 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 238, 100, 20, hWnd, NULL, NULL, NULL);
-	hKey24 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, 258, 100, 20, hWnd, NULL, NULL, NULL);
+	handleYpos = 38;
+	for (int i = 12; i < 24; i++)
+	{
+		hKey[i] = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 366, handleYpos, 100, 20, hWnd, NULL, NULL, NULL);
+		handleYpos += 20;
+	}
+
 
 	//Left Page Input
-	hPage1 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 38, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage2 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 58, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage3 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 78, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage4 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 98, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage5 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 118, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage6 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 138, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage7 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 158, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage8 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 178, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage9 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 198, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage10 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 218, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage11 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 238, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage12 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, 258, 20, 20, hWnd, NULL, NULL, NULL);
+	handleYpos = 38;
+	for (int i = 0; i < 12; i++)
+	{
+		hPage[i] = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 166, handleYpos, 20, 20, hWnd, NULL, NULL, NULL);
+		handleYpos += 20;
+	}
+
 	//Right Page Input
-	hPage13 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 38, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage14 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 58, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage15 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 78, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage16 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 98, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage17 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 118, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage18 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 138, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage19 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 158, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage20 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 178, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage21 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 198, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage22 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 218, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage23 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 238, 20, 20, hWnd, NULL, NULL, NULL);
-	hPage24 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, 258, 20, 20, hWnd, NULL, NULL, NULL);
+	handleYpos = 38;
+	for (int i = 12; i < 24; i++)
+	{
+		hPage[i] = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 486, handleYpos, 20, 20, hWnd, NULL, NULL, NULL);
+		handleYpos += 20;
+	}
 
 	//Left Line Input
-	hLine1 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 38, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine2 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 58, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine3 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 78, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine4 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 98, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine5 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 118, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine6 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 138, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine7 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 158, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine8 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 178, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine9 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 198, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine10 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 218, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine11 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 238, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine12 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, 258, 20, 20, hWnd, NULL, NULL, NULL);
+	handleYpos = 38;
+	for (int i = 0; i < 12; i++)
+	{
+		hLine[i] = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 206, handleYpos, 20, 20, hWnd, NULL, NULL, NULL);
+		handleYpos += 20;
+	}
 
 	//Right Line Input
-	hLine13 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 38, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine14 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 58, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine15 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 78, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine16 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 98, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine17 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 118, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine18 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 138, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine19 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 158, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine20 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 178, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine21 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 198, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine22 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 218, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine23 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 238, 20, 20, hWnd, NULL, NULL, NULL);
-	hLine24 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, 258, 20, 20, hWnd, NULL, NULL, NULL);
+	handleYpos = 38;
+	for (int i = 12; i < 24; i++)
+	{
+		hLine[i] = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 526, handleYpos, 20, 20, hWnd, NULL, NULL, NULL);
+		handleYpos += 20;
+	}
 
 	//Left Word Input
-	hWord1 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 38, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord2 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 58, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord3 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 78, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord4 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 98, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord5 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 118, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord6 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 138, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord7 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 158, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord8 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 178, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord9 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 198, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord10 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 218, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord11 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 238, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord12 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, 258, 20, 20, hWnd, NULL, NULL, NULL);
+	handleYpos = 38;
+	for (int i = 0; i < 12; i++)
+	{
+		hWord[i] = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 246, handleYpos, 20, 20, hWnd, NULL, NULL, NULL);
+		handleYpos += 20;
+	}
+
 
 	//Right Word Input
-	hWord13 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 38, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord14 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 58, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord15 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 78, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord16 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 98, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord17 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 118, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord18 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 138, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord19 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 158, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord20 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 178, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord21 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 198, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord22 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 218, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord23 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 238, 20, 20, hWnd, NULL, NULL, NULL);
-	hWord24 = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, 258, 20, 20, hWnd, NULL, NULL, NULL);
+	handleYpos = 38;
+	for (int i = 12; i < 24; i++)
+	{
+		hWord[i] = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, 566, handleYpos, 20, 20, hWnd, NULL, NULL, NULL);
+		handleYpos += 20;
+	}
+
 
 	//Button
 	HWND hBut = CreateWindowW(L"Button", L"Create", WS_VISIBLE | WS_CHILD | BS_BITMAP, 512, 284, 80, 120, hWnd, (HMENU)CREATE_BUTTON, NULL, NULL);
